a3fb70f26b08b5914b92bcab7f1da98d3a240065
hirodinn <hirebikila1@gmail.com>
2026-05-03 15:38:20 +0300

    initialize node application with Docker setup and express server

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) =>{
+    res.send("<h1>Hello there we are cool at working right now</h1>")
+})
+
+app.listen(3000, console.log("App running on port 3000..."))
